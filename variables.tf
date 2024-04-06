f0bb79e17d91f8155a08ed0c4cf79ddbc2aca58d
stefanioja <stefanvalentin.ioja@gmail.com>
2024-04-06 11:39:21 +0300

    destroy

--- variables.tf
+++ variables.tf
@@ -1,41 +1,41 @@
-# variable "aws_region" {
-#   type        = string
-#   description = "The region for resource deployment"
-#   default     = "eu-central-1"
-# }
+variable "aws_region" {
+  type        = string
+  description = "The region for resource deployment"
+  default     = "eu-central-1"
+}
 
-# variable "azs" {
-#   type        = list(string)
-#   description = "Availability zones for the region"
-#   default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-# }
+variable "azs" {
+  type        = list(string)
+  description = "Availability zones for the region"
+  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
 
-# variable "org" {
-#   type        = string
-#   description = "The organization, short name"
-#   default     = "FIIpractic"
-# }
+variable "org" {
+  type        = string
+  description = "The organization, short name"
+  default     = "FIIpractic"
+}
 
-# variable "env" {
-#   type        = string
-#   description = "Environment, e.g. prod, stage, dev"
-#   default     = "OIDC"
-# }
+variable "env" {
+  type        = string
+  description = "Environment, e.g. prod, stage, dev"
+  default     = "OIDC"
+}
 
-# variable "creator" {
-#   type        = string
-#   description = "Creator of resources, e.g. terraform"
-#   default     = "Managed by Terraform"
-# }
+variable "creator" {
+  type        = string
+  description = "Creator of resources, e.g. terraform"
+  default     = "Managed by Terraform"
+}
 
-# variable "stage" {
-#   type        = string
-#   description = "Stage for APIs, e.g. prod, stage, dev"
-#   default     = "FIIpractic"
-# }
+variable "stage" {
+  type        = string
+  description = "Stage for APIs, e.g. prod, stage, dev"
+  default     = "FIIpractic"
+}
 
-# variable "account_id" {
-#   type        = string
-#   description = "The AWS account ID for dev/tst/prod"
-#   default     = "125694839275"
-# }
+variable "account_id" {
+  type        = string
+  description = "The AWS account ID for dev/tst/prod"
+  default     = "125694839275"
+}
