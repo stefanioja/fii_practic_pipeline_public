f19fb03f93d1a73dbd77807e03ec31dc31d59100
stefanioja <stefanvalentin.ioja@gmail.com>
2024-04-06 10:40:41 +0300

    initial commit

--- backend.tf
+++ backend.tf
@@ -13,26 +13,26 @@ terraform {
 }
 
 
-# module "s3-terraform-state" {
-#   source  = "cloudposse/s3-bucket/aws"
-#   version = "3.1.3"
+ module "s3-terraform-state" {
+   source  = "cloudposse/s3-bucket/aws"
+   version = "3.1.3"
 
-#   bucket_name        = "s3-terraform-state-fii-practic-iuewygtiouwetf65"
-#   versioning_enabled = true
-# }
+   bucket_name        = "s3-terraform-state-fii-practic-tefaniuewygtiouwetxxf65"
+   versioning_enabled = true
+ }
 
 
-# resource "aws_dynamodb_table" "terraform_state_lock" {
-#   name         = "terraform-state-lock"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
+ resource "aws_dynamodb_table" "terraform_state_lock" {
+   name         = "terraform-state-lock"
+   billing_mode = "PAY_PER_REQUEST"
+   hash_key     = "LockID"
 
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-#   tags = {
-#     Environment = "CICD"
-#     Name        = "FIIpractic"
-#   }
-# }
+   attribute {
+     name = "LockID"
+     type = "S"
+   }
+   tags = {
+     Environment = "CICD"
+     Name        = "FIIpractic"
+   }
+ }
