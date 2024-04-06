c1123eea4b2e31663149d38059b4172ab958095f
stefanioja <stefanvalentin.ioja@gmail.com>
2024-04-06 10:59:01 +0300

    uncommented stuff

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-#terraform {
+# terraform {
 #  backend "s3" {
 #    bucket         = "s3-terraform-state-fii-practic-tefaniuewygtiouwetxxf65111"
 #    key            = "terraform.tfstate"
@@ -10,29 +10,29 @@ provider "aws" {
 #    encrypt        = true
 #    dynamodb_table = "terraform-state-lock"
 #  }
-#}
+# }
 
 
- module "s3-terraform-state" {
-   source  = "cloudposse/s3-bucket/aws"
-   version = "3.1.3"
+# module "s3-terraform-state" {
+#   source  = "cloudposse/s3-bucket/aws"
+#   version = "3.1.3"
 
-   bucket_name        = "s3-terraform-state-fii-practic-tefaniuewygtiouwetxxf65111"
-   versioning_enabled = true
- }
+#   bucket_name        = "s3-terraform-state-fii-practic-tefaniuewygtiouwetxxf65111"
+#   versioning_enabled = true
+# }
 
 
- resource "aws_dynamodb_table" "terraform_state_lock" {
-   name         = "terraform-state-lock"
-   billing_mode = "PAY_PER_REQUEST"
-   hash_key     = "LockID"
+# resource "aws_dynamodb_table" "terraform_state_lock" {
+#   name         = "terraform-state-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#    hash_key     = "LockID"
 
-   attribute {
-     name = "LockID"
-     type = "S"
-   }
-   tags = {
-     Environment = "CICD"
-     Name        = "FIIpractic"
-   }
- }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   tags = {
+#     Environment = "CICD"
+#     Name        = "FIIpractic"
+#   }
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,48 +1,48 @@
-# #region locals
+#region locals
 
-# locals {
-#   name   = "fii-practic-${var.env}"
-#   region = "eu-central-1"
+locals {
+  name   = "fii-practic-${var.env}"
+  region = "eu-central-1"
 
-#   vpc_cidr = "10.0.0.0/16"
-#   azs      = ["eu-central-1a", "eu-central-1b"]
+  vpc_cidr = "10.0.0.0/16"
+  azs      = ["eu-central-1a", "eu-central-1b"]
 
-#   tags = {
-#     Use = local.name
-#   }
-# }
+  tags = {
+    Use = local.name
+  }
+}
 
-# #endregion locals
+#endregion locals
 
-# #region vpc
+#region vpc
 
-# module "vpc" {
+module "vpc" {
 
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "5.0.0"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
 
-#   name = local.name
-#   cidr = local.vpc_cidr
+  name = local.name
+  cidr = local.vpc_cidr
 
-#   azs              = local.azs
-#   private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-#   public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
-#   database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
+  azs              = local.azs
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 8)]
 
-#   private_subnet_names  = ["Private Subnet One", "Private Subnet Two"]
-#   database_subnet_names = ["DB Subnet One"]
+  private_subnet_names  = ["Private Subnet One", "Private Subnet Two"]
+  database_subnet_names = ["DB Subnet One"]
 
-#   create_database_subnet_group  = true
-#   manage_default_network_acl    = false
-#   manage_default_route_table    = false
-#   manage_default_security_group = false
+  create_database_subnet_group  = true
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
 
-#   single_nat_gateway = true
-#   enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_nat_gateway = true
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# #endregion vpc
+#endregion vpc
 
-# # ✓ VPC
+# ✓ VPC
